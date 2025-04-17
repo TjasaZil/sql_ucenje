d11eef3648136b0523ebf69dda3a48abcbe97284
TjasaZil <tjasa.zilavec@gmail.com>
2025-04-17 18:59:23 +0200

    dodala dodatne naloge in spremenila sintakso na velike crke

--- vaje1/vaje1_query.sql
+++ vaje1/vaje1_query.sql
@@ -1,11 +1,11 @@
-create table if not exists student (
+CREATE TABLE IF NOT EXISTS student (
 	vpisna_st int,
 	ime varchar(20),
 	priimek varchar(20),
 	leto_rojstva int
 );
 
-create table if not exists predmet(
+CREATE TABLE IF NOT EXISTS predmet(
 ime_predmeta varchar(50),
 stevilo_ur int,
 datum_zacetka date,
@@ -13,7 +13,7 @@ datum_izpita datetime,
 kreditne_tocke int
 );
 
-create table if not exists predavatelj(
+CREATE TABLE IF NOT EXISTS predavatelj(
 ime_predavatelja varchar(20),
 priimek_predavatelja varchar(20),
 naziv varchar(20),
--- vaje1/vaje1_ukazi.sql
+++ vaje1/vaje1_ukazi.sql
@@ -1,61 +1,61 @@
-Select * from student;
+SELECT * FROM student;
 
 -- Izpišite vse podatke o vseh študentih z imenom Marcel.
-Select * 
-from student
-where ime = 'Marcel';
+SELECT * 
+FROM student
+WHERE ime = 'Marcel';
 
 -- Izpiši imena in vpisne številke vseh študentov, ki se pišejo Kolar.
-Select ime, vpisna_st
-from student
-where priimek ='Kolar';
+SELECT ime, vpisna_st
+FROM student
+WHERE priimek ='Kolar';
 
 -- Kako je ime študentu z vpisno številko 2417862?
-select ime
-from student
-where vpisna_st = 2417862;
+SELECT ime
+FROM student
+WHERE vpisna_st = 2417862;
 
 -- Katerega leta je rojen Emil Skok?
-select leto_rojstva
-from student
-where ime='Emil' and priimek='Skok';
+SELECT leto_rojstva
+FROM student
+WHERE ime='Emil' AND priimek='Skok';
  
--- Kakšna je vpisna številka Elvisa Javornika?
-select vpisna_st
-from student
-where ime ='Elvis' and priimek ='Javornik';
+-- Kakšna je vpisna številka Elvisa JavORnika?
+SELECT vpisna_st
+FROM student
+WHERE ime ='Elvis' AND priimek ='Javornik';
 
 -- Izpišite vse podatke o študentih, ki so rojeni po letu 1980.
-select *
-from student
-where leto_rojstva>1980;
+SELECT *
+FROM student
+WHERE leto_rojstva>1980;
 
 -- Izpišite vse podatke o študentkah z imenom Cvetka, ki so starejše od 50 let.
-select *
-from student
-where ime='Cvetka' and 2025-leto_rojstva > 50;
+SELECT *
+FROM student
+WHERE ime='Cvetka' AND 2025-leto_rojstva > 50;
 
 -- Izpišite imena in priimke vseh študentov, ki so rojeni med 1970 in 1980.
-select ime, priimek
-from student
-where leto_rojstva > 1970 and leto_rojstva < 1980;
+SELECT ime, priimek
+FROM student
+WHERE leto_rojstva > 1970 AND leto_rojstva < 1980;
 
 -- Izpišite vse podatke o študentih, ki so rojeni v letih 1970, 1980, 1990 in 2000.
-select *
-from student
-where leto_rojstva=1970 or leto_rojstva=1980 or leto_rojstva=1990 or leto_rojstva=2000;
+SELECT *
+FROM student
+WHERE leto_rojstva=1970 OR leto_rojstva=1980 OR leto_rojstva=1990 OR leto_rojstva=2000;
 
 -- Izpišite vse podatke o študentih, ki niso rojeni v letih 1970, 1980, 1990 in 2000.
-select *
-from student
-where not (leto_rojstva=1970 or leto_rojstva=1980 or leto_rojstva=1990 or leto_rojstva=2000);
+SELECT *
+FROM student
+WHERE NOT (leto_rojstva=1970 OR leto_rojstva=1980 OR leto_rojstva=1990 OR leto_rojstva=2000);
 
 -- Izpišite vse podatke o študentih, ki so rojeni ali 1998 ali 2000.
-select *
-from student
-where leto_rojstva = 1998 or leto_rojstva = 2000;
+SELECT *
+FROM student
+WHERE leto_rojstva = 1998 OR leto_rojstva = 2000;
 
 -- Ali obstaja študentka Tia Kolar rojena 1999?
-select *
-from student
-where ime = 'Tia' and priimek = 'Kolar'and leto_rojstva='1999';
+SELECT *
+FROM student
+WHERE ime = 'Tia' AND priimek = 'Kolar' AND leto_rojstva='1999';
