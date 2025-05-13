74acb0169b5e4d9ababc7ec686c4960abe09498a
TjasaZil <tjasa.zilavec@gmail.com>
2025-05-13 19:31:07 +0200

    .

--- vaje5/vaja3.sql
+++ vaje5/vaja3.sql
@@ -3,3 +3,17 @@ Izdelajte preprost(-e) sprožilec(-ce), ki preveri(-jo)
 nepravilne vnose v tabele za ljubljanski maraton.
 Preverite lahko ali so letnice tekmovalcev ustrezne
 */
+
+DELIMITER //
+
+CREATE TRIGGER prevent_age_error_maraton
+BEFORE INSERT ON tekac
+FOR EACH ROW
+BEGIN
+    IF  YEAR(NOW()) - YEAR(NEW.datum_rojstva) < 20 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Starost ne udelezenca mora biti vsaj 20 let!’;
+    END IF;
+END 
+//
+DELIMITER ;
