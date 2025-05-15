59ffeabe4b77f20bb12adc7642a9ef340081f00f
Tjasa <53261472+TjasaZil@users.noreply.github.com>
2025-05-15 15:01:42 +0200

    dodala jave 6

---
+++ vaje6/vaje.sql
@@ -0,0 +1,9 @@
+-- Izdelajte ER diagram za preprosto socialno omrežje. Poudarek naj bo na podatkih 'prijateljstva'. Izdelajte tabelo uporabniki in implementirajte povezave s katerimi bo možno ugotoviti kateri uporabniki so med seboj prijatelji. Upoštevajte, da so prijateljstva obojestranska.
+
+-- Dopolnite zgornji ER model tako, da si bosta dva uporabnika lahko izmenjevala sporočila. Vsa sporočila se seveda hranijo v tabeli skupaj z datumom in časom izdelave sporočila.
+
+-- Izdelajte vse tabele ter v vsako zapišite po nekaj podatkov.
+
+-- Zapišite poizvedbo, ki bo izpisala vse prijatelje "Tega in Tega" uporabnika.
+
+-- Zapišite poizvedbe s katerimi lahko poiščete in izpišete vsa sporočila, ki jih je poslal "Ta in Ta".
