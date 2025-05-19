a3e4faecedd20bb28873e9363667497462e73612
Tjasa <53261472+TjasaZil@users.noreply.github.com>
2025-05-19 11:31:44 +0200

    rešen titanik

--- dodatne_naloge/vse_vaje_ponavljanje.sql
+++ dodatne_naloge/vse_vaje_ponavljanje.sql
@@ -85,39 +85,95 @@ where g.name = 'Horror';
 
  -- passengers
  -- Izračunaj povprečno starost vseh potnikov.
+ select avg(age) as avg_age from passengers;
+
 -- Izračunaj skupno število potnikov.
+select count(*) as all_passengers from passengers;
+
 -- Izračunaj povprečno ceno vozovnice za potnike, ki so potovali v 2. razredu.
+select avg(fare) as avg_ticket from passengers where pclass = 2;
+
 -- Izračunaj skupni prihodek iz vozovnic potnikov, ki so potovali iz pristanišča 'S'.
+select sum(fare) as sum_fare from passengers where embarked = 'S';
+
 -- Izračunaj povprečno starost preživelih potnikov.
+select avg(age) as avg_age from passengers where survived = 1;
+
 -- Preštej, koliko potnikov je bilo žensk.
+select count(*) as count_female from passengers where sex = 'female';
+
 -- Preštej, koliko potnikov je bilo mlajših od 18 let.
+select count(*) from passengers where age < 18;
+
 -- Izračunaj povprečno ceno vozovnice za moške potnike, ki niso preživeli.
+select avg(fare) as avg_fare from passengers where sex = 'male' and survived = 0;
+
 -- Izračunaj skupno število potnikov, ki so potovali v 3. razredu in niso imeli kabine.
+select count(*) as count_passengers from passengers where pclass=3 and cabin = '';
+
 -- Izračunaj skupno število preživelih potnikov, ki so potovali iz pristanišča 'Q'.
+select count(*) as count_passengers from passengers where survived = 1 and embarked = 'Q';
+
 -- Izračunaj povprečno ceno vozovnice za preživele potnike, ki so potovali v 1. razredu.
+select count(*) as count_passengers from passengers where pclass = 1 and survived = 1;
+
 -- Izračunaj skupno število moških potnikov, ki so bili mlajši od 25 let in niso preživeli.
+select count(*) as count_passengers from passengers where sex = 'male' and survived = 0 and age < 25;
+
 -- Izračunaj povprečno starost žensk, ki so potovale iz pristanišča 'C'.
+select count(*) as count_passengers from passengers where sex = 'female'  and embarked = 'C';
+
 -- Najdi maksimalno starost med potniki, ki so potovali v 3. razredu in niso imeli kabine.
+select max(age) from passengers where pclass=3 and cabin = '';
+
 -- Izračunaj skupno število potnikov, ki so potovali iz pristanišča 'S' in so plačali manj kot 10 za vozovnico.
+select count(*) from passengers where embarked = 'S' and fare <10;
+
 -- Poišči najmanjšo ceno vozovnice med vsemi preživelimi potniki.
+select min(fare) from passengers where survived = 1;
+
 -- Izračunaj povprečno ceno vozovnice za potnike, starejše od 40 let, ki niso imeli določene kabine.
+select avg(fare) from passengers where age > 40 and cabin = '';
+
 -- Izračunaj povprečno starost potnikov, ki so potovali s pristanišča 'Q' in so plačali več kot 20 za vozovnico.
+select avg(age) from passengers where embarked = 'Q' and fare > 20;
+
 -- Najdi najvišjo ceno vozovnice med ženskami, ki so potovale v 2. razredu.
+select max(fare) from passengers where pclass = 2 and sex = 'female';
+
 -- Izračunaj skupno število potnikov brez kabine, ki so bili starejši od 30 let in niso preživeli.
+select count(*) from passengers where survived = 0 and age > 30 and cabin = '';
 -- Koliko filmov je bilo izdanih v vsakem žanru po letu 2010?
+
 -- Koliko filmov je bilo izdanih v vsakem letu?
+
 -- Za vsak mpaa_rating izpiši povprečno oceno filma.
+
 -- Koliko je zaslužil vsak žanr? Uredite od tistega z največjim zaslužkom do tistega z najnižjim.
+
 -- Kakšna je povprečna dolžina filma po letih?
+
 -- Kakšno je bilo skupno število ocen v vsakem žanru?
+
 -- Koliko v povprečju trajajo filmi različnih žanrov?
+
 -- Koliko je skupnih ocen (rating_count) vsakega žanra?
+
 -- Kakšna je bila povprečna cena vozovnice v posameznem cenovnem razredu?
--- Kakšen je bil odstotek preživelih moških in žensk?
+Select avg(fare), pclass from passengers group by pclass;
+
 -- Kakšna je bila povprečna starost preživelega in ne-preživelega potnika?
+select avg(age), survived from passengers group by survived;
+
 -- Koliko ljudi je bilo v posameznem rešilnem čolnu?
+select count(*), boat from passengers group by boat;
+
 -- Koliko potnikov niso nikoli našli?
+select count(*) from passengers where body = 0;
+
 -- Koliko potnikov se je vkrcalo na posameznem pristanišču?
+select count(embarked) as embarked_group from passengers
+group by embarked;
 
 -- vaje 3 -- movies, mpaa_ratings, genres, characters, actors
 -- inner join in brez
